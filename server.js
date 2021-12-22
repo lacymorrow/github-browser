97ea2d7ae3a8214eee6fb7aa1dcf85972b510846
Lacy Morrow <me@lacymorrow.com>
2021-12-22 12:11:41 -0800

    allow custom port

--- server.js
+++ server.js
@@ -6,26 +6,26 @@ const app = next({ dev })
 const handle = app.getRequestHandler()
 
 app
-	.prepare()
-	.then(() => {
-		const port = 3000
-		const server = express()
+    .prepare()
+    .then(() => {
+        const port = process.env.PORT || 3000
+        const server = express()
 
-		server.get('/repo/:repo', (req, res) => {
-		  const actualPage = '/'
-		  const queryParams = { repo: req.params.repo } 
-		  app.render(req, res, actualPage, queryParams)
-		})
+        server.get('/repo/:repo', (req, res) => {
+            const actualPage = '/'
+            const queryParams = { repo: req.params.repo }
+            app.render(req, res, actualPage, queryParams)
+        })
 
-		server.get('*', (req, res) => {
-			return handle(req, res)
-		})
-		server.listen(port, err => {
-			if (err) throw err
-			console.log(`> Ready on http://localhost:${port}`)
-		})
-	})
-	.catch(ex => {
-		console.error(ex.stack)
-		process.exit(1)
-	})
+        server.get('*', (req, res) => {
+            return handle(req, res)
+        })
+        server.listen(port, err => {
+            if (err) throw err
+            console.log(`> Ready on http://localhost:${port}`)
+        })
+    })
+    .catch(ex => {
+        console.error(ex.stack)
+        process.exit(1)
+    })
